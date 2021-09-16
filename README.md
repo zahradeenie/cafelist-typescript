f44ece8f39ef98d4c9a8ba1c96ed7cc19cea21ea
Zahra <zahradeenie@gmail.com>
2021-09-16 20:22:55 +0200

    initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cafelist in TypeScript
+
+re-working the cafelist project in typescript to learn typescript, innit
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const earth = 'world';
+
+export function hello(world: string = earth): string {
+  return `Hello ${world}! `;
+}
